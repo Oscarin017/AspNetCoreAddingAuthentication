a3b295f58bb97fbe7e3d5780fa2987c99151e5ae
Oscar GONZALEZ <OGO@sfinternal.private>
2019-07-22 15:42:05 +0200

    Completed module.

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace WishList.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
